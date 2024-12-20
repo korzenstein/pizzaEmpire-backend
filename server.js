65646ec327096f6b91716df942b5fb1470de2168
Stephen Korzenstein <skorzenstein@gmail.com>
2024-12-20 11:05:14 -0500

    add logs to cors

--- server.js
+++ server.js
@@ -15,9 +15,11 @@ const allowedOrigins = [
 app.use(
   cors({
     origin: function (origin, callback) {
+      console.log(`Request Origin: ${origin}`);
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
+        console.error(`Blocked by CORS: ${origin}`);
         callback(new Error("Not allowed by CORS"));
       }
     },
