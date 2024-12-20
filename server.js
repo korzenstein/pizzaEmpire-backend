b3b5369b41f180e0ed5ae49ae72eeeeaddee1ca2
Stephen Korzenstein <skorzenstein@gmail.com>
2024-12-19 22:43:05 -0500

    add support for netlify

--- server.js
+++ server.js
@@ -6,18 +6,34 @@ require("dotenv").config();
 
 const app = express();
 // Enable CORS
-app.use(cors({
-  origin: "http://localhost:3000", 
-}));
+const allowedOrigins = [
+  "http://localhost:3000", // Local frontend during development
+  process.env.FRONTEND_URL, // Frontend deployed on Netlify
+];
 
-app.use(express.json()); // Parse JSON bodies
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (e.g., mobile apps, curl requests)
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
+
+app.use(express.json());
 
 // Routes
 app.use("/players", playersRoutes);
 app.use("/inventory", inventoryRoutes);
 
 // Start the server
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
